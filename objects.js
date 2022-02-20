f2bbdf169d0ca036b44aa8d33f30d65327441f0e
Celena Rock <92170269+celenaRock@users.noreply.github.com>
2022-02-20 15:57:12 -0600

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,39 @@
+const { By, until, WebDriver } = require('selenium-webdriver');
+const chromedriver = require('chromedriver')
+
+module.exports = class BasePage {
+    driver;
+    url;
+
+    constructor(myDriver, myUrl) {
+        this.driver = myDriver
+        this.url = myUrl
+    }
+
+    async navigate() {
+        return await this.driver.get(this.url)
+    }
+
+    async getElement(elementBy) {
+        await this.driver.wait(until.elementLocated(elementBy));
+        let element = await this.driver.findElement(elementBy);
+        await this.driver.wait(until.elementLocated(elementBy));
+        return element;
+    }
+
+
+    async click(elementBy) {
+        return(await this.getElement(elementBy)).click()
+    }
+
+
+    async setInput(elementBy, keys) {
+        let input = await this.getElement(elementBy);
+        await input.clear();
+        return input.sendKeys(keys)
+    }
+
+    async getText(elementBy) {
+        return(await this.getElement(elementBy)).getText();
+    }
+}
